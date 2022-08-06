d74964d5162b0067f632bbb4ea0efdb46ced2ad8
sizan378 <sizanmahmud08@gmail.com>
2022-08-06 20:06:53 +0600

    add some whitenoise file

--- blog/urls.py
+++ blog/urls.py
@@ -35,5 +35,3 @@ urlpatterns = [
     path('api/v1/userprofile',include('userprofile.urls')),
     path('api/v1/account/',include('account.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)

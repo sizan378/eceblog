64b04a2574baadd4a3a844003660615bb01b9234
sizan378 <sizanmahmud08@gmail.com>
2022-09-26 15:48:28 +0600

    commets app add in main app

--- blog/urls.py
+++ blog/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     path('api/v1/post/', include('post.urls')),
     path('api/v1/userprofile/', include('userprofile.urls')),
     path('api/v1/account/', include('account.urls')),
+    path('api/v1/comments/', include('comments.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

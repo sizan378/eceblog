e57e3fa6e698a091336befefc21f9d4e861a804d
sizan378 <sizanmahmud08@gmail.com>
2022-09-27 16:07:43 +0600

    add some code

--- comments/views.py
+++ comments/views.py
@@ -5,11 +5,14 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from comments.models import CommentsModel
 from comments.serializers import CommentSerializers
 
+
 class ListCreateView(ListCreateAPIView):
-    serializer_class = CommentSerializers
     queryset = CommentsModel.objects.all()
+    serializer_class = CommentSerializers
 
 
 class RetrieveUpdateView(RetrieveUpdateAPIView):
     serializer_class = CommentSerializers
     queryset = CommentsModel.objects.all()
+
+    serializer = CommentSerializers(queryset, many=True)
--- post/models.py
+++ post/models.py
@@ -13,10 +13,10 @@ class ArticleModel(models.Model):
     author_name = models.CharField(max_length=200)
     title = models.CharField(max_length=100)
     body = models.TextField()
-    image = models.ImageField(upload_to='media', verbose_name='Image')
+    image = models.ImageField(upload_to='image', verbose_name='Image')
     category = models.ForeignKey(CategoryModel, on_delete=models.DO_NOTHING)
     status = models.BooleanField(default=False)
-    comment = models.ForeignKey(CommentsModel, on_delete=models.DO_NOTHING)
+    # comment = models.ForeignKey(CommentsModel, on_delete=models.DO_NOTHING)
     created_at = models.DateField()
     updated_at = models.DateField()
 

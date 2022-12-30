e4c031c2abba9953f1214c1919f2eec3df8f7a31
sizan378 <sizan378>
2022-12-30 19:29:44 +0600

    🦄 refactor(code repath): code repath
    code repath

--- comments/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from comments.models import CommentsModel
-
-
-class CommentsAdmin(admin.ModelAdmin):
-    list_display = ('full_name','comments','created_at')
-    search_fields = ['full_name','comments']
-    list_per_page = 25 # No of records per page
-admin.site.register(CommentsModel,CommentsAdmin)
--- notice/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import NoticeModel
-
-
-class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('id','notice')
-admin.site.register(NoticeModel, NoticeAdmin)

9da303ff43728a08d8a21e0fd2e7fdfd07b81330
sizan378 <sizan378>
2023-01-02 14:39:44 +0600

    ✨ feat(settings files): setting
    settings file setup

---
+++ blog/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}

c609c68bb3cf0725b127142b02eb2135ecdb2551
sizan378 <sizan378>
2023-01-08 16:44:57 +0600

    🦄 refactor(models.py): apps/post
    remove image update location and add auto-now true

--- apps/post/models.py
+++ apps/post/models.py
@@ -13,12 +13,13 @@ class ArticleModel(models.Model):
     author_name = models.CharField(max_length=200)
     title = models.CharField(max_length=100)
     body = models.TextField()
-    image = models.ImageField(upload_to='image', verbose_name='Image')
+    image = models.ImageField(
+        verbose_name='Image', null=True, blank=True)
     category = models.ForeignKey(CategoryModel, on_delete=models.DO_NOTHING)
     status = models.BooleanField(default=False)
     # comment = models.ForeignKey(CommentsModel, on_delete=models.DO_NOTHING)
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title

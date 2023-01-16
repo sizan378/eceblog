ed01f2b6318db51a9680771497c5cc7b1b144436
sizan378 <sizan378>
2023-01-16 18:10:07 +0600

    🦄 refactor(views.py): apps/users token for user add

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,16 +3,27 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializer import CustomUserSerializer, CustomUserLogin
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 class UserRegistration(APIView):
     def post(self, request, format=None):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response(serializer.data)
+            token = get_tokens_for_user(user)
+            return Response({"token": token})
         return Response(serializer.error_messages)
 
 class UserLogin(APIView):
@@ -23,9 +34,10 @@ class UserLogin(APIView):
             password = serializer.data.get('password')
             user = authenticate(email=email, password=password)
             if user is not None:
-                return Response("please provide a username and password")
+                token = get_tokens_for_user(user)
+                return Response({"token": token})
             else:
-                return Response("user login success")
+                return Response("please provide email and password")
 
 # class UserRegistration(TokenObtainPairView):
 #     '''For admin Get access token & refresh token
